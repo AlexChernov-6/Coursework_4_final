0515b17b47e5c3891b5ed5004059b88efb8a6795
AlexChernov-6 <151832804+AlexChernov-6@users.noreply.github.com>
2025-12-23 12:11:14 +0300

    Delete Чернов Алексей/Coursework-main/feedback_on_school_meals directory

--- Чернов Алексей/Coursework-main/feedback_on_school_meals/src/main/java/com/example/feedback_on_school_meals/service/DailyMenusService.java
+++
@@ -1,9 +0,0 @@
-package com.example.feedback_on_school_meals.service;
-
-import com.example.feedback_on_school_meals.model.DailyMenus;
-
-public class DailyMenusService extends BaseService<DailyMenus> {
-    public DailyMenusService() {
-        super(DailyMenus.class);
-    }
-}
